f137a09be6b72ffdedb160fc2d3c3b444b3e4615
sorasen <112673854+sorasen2020@users.noreply.github.com>
2023-01-29 11:51:15 +0900

    v0.1リリース

---
+++ PokeControllerForESP32.ino
@@ -0,0 +1,48 @@
+#include "PokeControllerForESP32_Func.h"
+
+int ProgState = STATE0;
+static char pc_report_str[MAX_BUFFER];
+static uint8_t idx = 0;
+
+void setup() {
+  Serial_Init();     // RX←0でのシリアル通信
+  Controller_Init(); // コントローラーの準備
+  //Keyboard_Init();   // キーボードの初期化
+  USB.begin();
+  Controller_Reset();
+}
+
+void loop() {
+  //状態に応じて切り替える
+  switch (ProgState)
+  {
+    case STATE1:
+      SwitchFunction();
+      break;
+    default:
+      /* バグ回避を兼ねて状態の初期化を行う */
+      ProgState = STATE0;
+      break;
+  }
+}
+
+//データが利用可能な時に呼び出される関数(Serial1)
+void serialEvent()
+{
+  // one character comes at a time
+  char c = Serial.read();
+
+  if (c == '\n')
+  {
+    pc_report_str[idx++] = c;
+    pc_report_str[idx++] = '\0';
+    ParseLine(pc_report_str);
+    idx = 0;
+    memset(pc_report_str, 0, sizeof(pc_report_str));
+    ProgState = STATE1;
+  }
+  else if (idx < MAX_BUFFER)
+  {
+    pc_report_str[idx++] = c;
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # PokeControllerForESP32
+
+[Poke Controller Modified](https://github.com/Moi-poke/Poke-Controller-Modified)とSwitch間を中継するソフトウェア
+
+# このソフトウェアについて
+
+ろっこく氏のPoke-ControllerForLeonardoを参考にして作っています
+
+# 導入方法
+
+必要なもの
+
+    ESP32-S3-DevkitC-1 1台(https://akizukidenshi.com/catalog/g/gM-17073/)
+    USB マイクロケーブル 2本
+
+動作環境
+
+    Arduino IDE(1.8.19推奨)
+    Arduino core for ESP32 (2.0.6)
+    SwitchControllerESP32
+
+# 使い方
+
+## ソフトウェアのダウンロード
+
+このリポジトリをダウンロードし適当なフォルダへ展開
+
+## 書き込み
+
+1. ESP32 S3 DevkitC-1のUSB端子のUARTシルク側をPCに接続、USBシルク側をSwitchへ接続
+2. Arduino IDEを立ち上げる
+3. ツール→ボード→ESP32 Arduino→ESP32S3 Dev Moduleを選択
+4. シリアルポートも接続してるポートに変更
+5. ファイル→開くでスケッチ(PokeControllerForESP32.ino)を開く
+6. スケッチ→マイコンボードへ書き込む
+
+Poke Controller側のCOM Port番号をESP32 S3 DevkitC-1のものに合わせてSwitch Controller Simulatorなどでボタンを押して動作確認してください
+
+# 注意
+
+まだキーボード入力には対応してません
+
+# Lisence
+
+このプロジェクトのライセンスはMITライセンスです。詳細はLICENSEをご覧ください
